ff2b3f6b585f711c1cfa3b92b977c98ddc977c84
erickbrdev <erick.dev07@gmail.com>
2023-05-03 16:55:01 -0300

    adição de projetos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,22 +14,22 @@ export const projects: IProjects[] = [
   },
   {
     id: uuidv4(),
-    title: "Jazz Coffee",
+    title: "Pokerick",
     description:
-      "A aplicação permite aos usuários navegarem por uma seleção de cafés, adicioná-los ao carrinho de compras e finalizar o pedido. Além disso, os usuários podem visualizar detalhes dos produtos, como nome, preço, descrição e imagem.",
+      "Este é um projeto desenvolvido com React, utilizando o Vite como ferramenta de desenvolvimento e Typescript para tipagem. A aplicação consiste em uma lista de Pokémons, obtida através de uma API externa.",
     urlProject:
-      "https://cafeteria-jazz-coffee-btyc50kiz-erickbrdev.vercel.app/",
-    urlGitHub: "https://github.com/erickbrdev/cafeteria-jazzCoffee",
-    img: "https://i.postimg.cc/T1JYCv32/Captura-de-tela-de-2023-04-28-15-34-33.png",
+      "https://poke-api-sable-two.vercel.app/",
+    urlGitHub: "https://github.com/erickbrdev/poke-api",
+    img: "https://i.postimg.cc/T1XfGXsc/Captura-de-tela-de-2023-05-03-10-01-59.png",
   },
   {
     id: uuidv4(),
-    title: "Jazz Coffee",
+    title: "Kcire Pets",
     description:
-      "A aplicação permite aos usuários navegarem por uma seleção de cafés, adicioná-los ao carrinho de compras e finalizar o pedido. Além disso, os usuários podem visualizar detalhes dos produtos, como nome, preço, descrição e imagem.",
+      "Aplicação moderna e responsiva. Desenvolvido com o objetivo de proporcionar uma experiência de usuário intuitiva e agradável, permitindo que os clientes possam navegar facilmente pelos produtos e serviços oferecidos pelo pet shop.",
     urlProject:
-      "https://cafeteria-jazz-coffee-btyc50kiz-erickbrdev.vercel.app/",
-    urlGitHub: "https://github.com/erickbrdev/cafeteria-jazzCoffee",
-    img: "https://i.postimg.cc/T1JYCv32/Captura-de-tela-de-2023-04-28-15-34-33.png",
+      "https://petshop-frontend-ml4wc6y77-erickbrdev.vercel.app/",
+    urlGitHub: "https://github.com/erickbrdev/petshop-frontend",
+    img: "https://i.postimg.cc/qv5qFZ0p/Captura-de-tela-de-2023-05-03-16-52-40.png",
   },
 ];
